19a3c5633a69540e7af3a61b5a8d5d26f8dab1ba
redwill <hro.2016@yandex.ru>
2026-03-01 13:01:59 +0400

    Remove unneccessary argument

--- utils/set_image_data.js
+++ utils/set_image_data.js
@@ -5,6 +5,6 @@ const shellVersion = parseInt(Config.PACKAGE_VERSION.split('.')[0]);
 // Assign correct implementation once
 export const setImageData = shellVersion >= 48
     ? (image, coglContext, data, format, width, height, rowstride) => 
-        image.set_data(coglContext, data, format, width, height, rowstride, null)
+        image.set_data(coglContext, data, format, width, height, rowstride)
     : (image, coglContext, data, format, width, height, rowstride) => 
         image.set_data(data, format, width, height, rowstride); // Gnome <48 doesnt require coglContext
